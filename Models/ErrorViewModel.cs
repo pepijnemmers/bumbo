0b06e12f71635e4c80332ccba5424c61c6576b86
pepijnemmers <pepijn.emmers@hotmail.com>
2024-10-03 21:59:08 +0200

    Project setup with MVC and necessary dependencies

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BumboApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
