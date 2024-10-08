4485a16df1a264a2259d8a97b0f325ad9879d173
SemvandenBos <semvandenbos1@gmail.com>
2024-10-08 16:04:32 +0200

    Updated Context class and added ContextFactory

--- Models/BumboDbContext.cs
+++ Models/BumboDbContext.cs
@@ -15,6 +15,20 @@ public partial class BumboDbContext : DbContext
     {
     }
 
+    public virtual DbSet<Expectation> Expectations { get; set; }
+
+    public virtual DbSet<Norm> Norms { get; set; }
+
+    public virtual DbSet<OpeningHour> OpeningHours { get; set; }
+
+    public virtual DbSet<Prognosis> Prognoses { get; set; }
+
+    public virtual DbSet<UniqueDay> UniqueDays { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    public virtual DbSet<WeekPrognosis> WeekPrognoses { get; set; }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlServer("Server=localhost;Database=BumboDb;Trusted_Connection=True;Encrypt=False;");
---
+++ Models/ContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+
+namespace BumboApp.Models
+{
+    public class ContextFactory : IDesignTimeDbContextFactory<BumboDbContext>
+    {
+        public ContextFactory()
+        {
+        }
+
+        private IConfiguration Configuration => new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+          .AddJsonFile("appsettings.json")
+          .Build();
+
+        public BumboDbContext CreateDbContext(string[] args)
+        {
+
+            var builder = new DbContextOptionsBuilder<BumboDbContext>();
+            builder.UseSqlServer(Configuration.GetConnectionString("FirstAndSecond"));
+
+            return new BumboDbContext(builder.Options);
+        }
+    }
+}
