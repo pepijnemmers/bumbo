3c06ffdcdd0286398e62a269ba35fe89ebdb2750
pepijnemmers <pepijn.emmers@hotmail.com>
2024-10-09 13:27:49 +0200

    Removed unused page and controller and added error catch in main controller

--- Controllers/MainController.cs
+++ Controllers/MainController.cs
@@ -1,4 +1,5 @@
-﻿using BumboApp.Models;
+﻿using System.Diagnostics;
+using BumboApp.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BumboApp.Controllers
@@ -9,6 +10,12 @@ namespace BumboApp.Controllers
         public MainController()
         {
             _context = new BumboDbContext();
+        }
+        
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
