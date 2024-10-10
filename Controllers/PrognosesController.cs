4f4befaaead590e8ac3267c2ac27811d2e890ddd
MatthijsCr <msj.cromwijk@student.avans.nl>
2024-10-10 15:41:22 +0200

    frontend works and takes info out of the DB, but code needs some cleaning up

--- Controllers/PrognosesController.cs
+++ Controllers/PrognosesController.cs
@@ -1,12 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Azure;
+using BumboApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
 
 namespace BumboApp.Controllers
 {
     public class PrognosesController : MainController
     {
-        public IActionResult Index()
+        BumboDbContext DbContext = new BumboDbContext();
+        public IActionResult Index(int? page)
         {
-            return View();
+            List<WeekPrognosis> Prognoses = DbContext.WeekPrognoses.ToList();
+            List<WeekPrognosis> PageList = new List<WeekPrognosis>();     
+
+            int PageNumber = page ?? 1;
+            int PageSize = 5;
+            int MaxPages = (int)(Math.Ceiling((decimal)Prognoses.Count / PageSize));
+            if(PageNumber <= 0) { PageNumber =1; }
+            if(PageNumber > MaxPages) { PageNumber = MaxPages; }
+
+            for(int i = (PageNumber-1)*PageSize; i <= PageNumber*PageSize && i < Prognoses.Count; i++)
+            {
+                PageList.Add(Prognoses[i]);
+            }
+
+            ViewBag.PageNumber = PageNumber;
+            ViewBag.PageSize = PageSize;
+            ViewBag.MaxPages = MaxPages;
+            return View(PageList);
         }
         public IActionResult Details()
         {
