4cb122f3c23a347f5f805884608394556637ac2e
SemvandenBos <semvandenbos1@gmail.com>
2024-10-11 11:37:32 +0200

    Added content for Prognoses/Update and detail

--- Controllers/PrognosesController.cs
+++ Controllers/PrognosesController.cs
@@ -1,6 +1,8 @@
 ﻿using BumboApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Globalization;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace BumboApp.Controllers
 {
@@ -10,18 +12,70 @@ namespace BumboApp.Controllers
         {
             return View();
         }
-        public IActionResult Details(int id)
+        public IActionResult Details(string id)
         {
-            WeekPrognosis wp = null;
-            foreach (WeekPrognosis selectedWp in _context.WeekPrognoses.Include(wp => wp.Prognoses))
-            {
-                if (selectedWp.Id == id) wp = selectedWp;
-            }
+            var parts = id.Split('-');
+            int year = int.Parse(parts[0]);
+            int weekNumber = int.Parse(parts[1]);
+
+            DateOnly mondayOfWeek = GetMondayOfWeek(year, weekNumber);
+
+            WeekPrognosis? wp = _context.WeekPrognoses
+        .Include(wp => wp.Prognoses)
+            .SingleOrDefault(wp => wp.StartDate == mondayOfWeek);
+
+            ViewData["Year"] = year;
+            ViewData["WeekNr"] = weekNumber;
+
             return View(wp);
         }
-        public IActionResult Update()
+        public IActionResult Update(string id)
         {
-            return View();
+            var parts = id.Split('-');
+            int year = int.Parse(parts[0]);
+            int weekNumber = int.Parse(parts[1]);
+
+            DateOnly mondayOfWeek = GetMondayOfWeek(year, weekNumber);
+
+            WeekPrognosis? wp = _context.WeekPrognoses
+        .Include(wp => wp.Prognoses)
+            .SingleOrDefault(wp => wp.StartDate == mondayOfWeek);
+
+            ViewData["Year"] = year;
+            ViewData["WeekNr"] = weekNumber;
+
+            return View(wp);
+        }
+
+        [HttpPost]
+        public IActionResult Update(List<Prognosis> prognoses)
+        {
+            //MOdelstate?
+
+            foreach (var prognosis in prognoses)
+            {
+                var existingPrognosis = _context.Prognoses
+                .Single(p => p.Department == prognosis.Department && p.Date == prognosis.Date);
+
+                if (existingPrognosis != null)
+                {
+                    existingPrognosis.NeededHours = prognosis.NeededHours;
+                    existingPrognosis.NeededEmployees = prognosis.NeededEmployees;
+                }
+            }
+
+            //_context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public DateOnly GetMondayOfWeek(int year, int weekNumber)
+        {
+            DateOnly jan1 = new DateOnly(year, 1, 1);
+            int daysOffset = DayOfWeek.Monday - jan1.DayOfWeek;
+            DateOnly firstMonday = jan1.AddDays(daysOffset);
+
+            return firstMonday.AddDays((weekNumber - 1) * 7);
         }
     }
 }
