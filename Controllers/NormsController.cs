209bb33ca60bd3fbfca2ff9778bcb71047e897ee
fvanhastenberg <f.vanhastenberg@student.avans.nl>
2024-10-15 17:54:46 +0200

    form temporary naar onderaan de pagina & probeersel

--- Controllers/NormsController.cs
+++ Controllers/NormsController.cs
@@ -19,17 +19,34 @@ namespace BumboApp.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Add()
+        public IActionResult Add(List<AddNormViewModel> nieuweNormeringen)
         {
-            //var newNormEntry = new Norm
+            //using var transaction = _context.Database.BeginTransaction();
+
+            //try
             //{
-            //    Activity = norm.Key,
-            //    Value = norm.Value.Value,
-            //    NormType = GetNormTypeForActivity(norm.Key),
-            //    CreatedAt = DateTime.Now
-            //};
-            //_context.Norms.Add(newNormEntry);
-            // _context.SaveChanges();
+            //    foreach (var item in nieuweNormeringen)
+            //    {
+            //        var newNormEntry = new Norm
+            //        {
+            //            Activity = item.NormActivity,
+            //            Value = item.Value,
+            //            NormType = item.NormType,
+            //            CreatedAt = DateTime.Now
+            //        };
+            //        _context.Norms.Add(newNormEntry);
+            //    }
+            //    _context.SaveChanges();
+            //    transaction.Commit();
+            //    NotifyService.Success("De normeringen zijn opgeslagen!");
+            //}
+            //catch
+            //{
+            //    transaction.Rollback();
+            //    NotifyService.Error("Er is iets mis gegaan");
+            //}
+
+            NotifyService.Success("De normeringen zijn opgeslagen!");
             return RedirectToAction("Index");
         }      
     }
--- ViewModels/NormsViewModel.cs
+++ ViewModels/NormsViewModel.cs
@@ -7,4 +7,11 @@ namespace BumboApp.ViewModels
         public required List<Norm> NormsList { get; set; }
         public required List<Norm> LatestNormsList { get; set; }
     }
+
+    public class AddNormViewModel
+    {
+        public NormActivity NormActivity { get; set; }
+        public int Value { get; set; }
+        public NormType NormType { get; set; }
+    }
 }
