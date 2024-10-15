fd003c31c9911a5203bca2d1bf7f3ae75b164af8
pepijnemmers <pepijn.emmers@hotmail.com>
2024-10-15 12:04:53 +0200

    added pagination to norms

--- Controllers/NormsController.cs
+++ Controllers/NormsController.cs
@@ -7,16 +7,35 @@ namespace BumboApp.Controllers
 {
     public class NormsController : MainController
     {
-        public IActionResult Index()
+        private static readonly int PageSize = Configuration.GetValue<int>("Pagination:DefaultPageSize");
+        private static readonly int DefaultPage = Configuration.GetValue<int>("Pagination:StartPage");
+
+        public IActionResult Index(int? page)
         {
+            List<Norm> norms = Context.Norms.OrderBy(n => n.CreatedAt).Skip(5).ToList();
+            
+            int currentPageNumber = page ?? DefaultPage;
+            int maxPages = (int)(Math.Ceiling((decimal)norms.Count / PageSize));
+            if (currentPageNumber <= 0) { currentPageNumber = DefaultPage; }
+            if (currentPageNumber > maxPages) { currentPageNumber = maxPages; }
+            
+            List<Norm> normsForPage = 
+                norms
+                .Skip((currentPageNumber - 1) * PageSize)
+                .Take(PageSize * 5)
+                .ToList();
+            
+            ViewBag.PageNumber = currentPageNumber;
+            ViewBag.PageSize = PageSize;
+            ViewBag.MaxPages = maxPages;
+            
             var viewModel = new NormsViewModel
             {
-                NormsList = Context.Norms.OrderBy(n => n.CreatedAt).Skip(5).ToList(),
+                NormsList = normsForPage,
                 LatestNormsList = Context.Norms.OrderBy(n => n.CreatedAt).Take(5).ToList()
             };
-            return View(viewModel);
-
             
+            return View(viewModel);
         }
     }
 }
