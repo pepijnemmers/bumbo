7919d83a646119f93b18fb7145512c5a5f3bdfc0
MatthijsCr <msj.cromwijk@student.avans.nl>
2024-10-16 16:39:52 +0200

    update mostly made

--- Controllers/UniqueDaysController.cs
+++ Controllers/UniqueDaysController.cs
@@ -5,10 +5,10 @@ namespace BumboApp.Controllers
 {
     public class UniqueDaysController : MainController
     {
-        public IActionResult Edit(int id)
+        public IActionResult Edit(int id )
         {
             UniqueDay? uniqueDay = Context.UniqueDays.Find(id);
-            return uniqueDay == null ? NotifyErrorAndRedirect("De Speciale dag die je probeert te bewerken bestaat niet.", "Index") : View(uniqueDay);
+            return uniqueDay == null ? NotifyErrorAndRedirect("De Speciale dag die je probeert te bewerken bestaat niet.", "Index", "OpeningHours") : View(uniqueDay);
         }
 
         public IActionResult Add()
@@ -21,34 +21,38 @@ namespace BumboApp.Controllers
         {
             // validation
             if (uniqueDay.StartDate > uniqueDay.EndDate)
-                return NotifyErrorAndRedirect("EindDatum mag niet voor de StartDatum liggen.", "Edit");
+                return NotifyErrorAndRedirect("EindDatum mag niet voor de StartDatum liggen.", "Index", "OpeningHours");
 
             if (Read().Find(u => u.StartDate == uniqueDay.StartDate && u.Name == uniqueDay.Name && uniqueDay.Id != u.Id) != null)
-                return NotifyErrorAndRedirect("Deze speciale dag bestaat al.", "Edit");
+                return NotifyErrorAndRedirect("Deze speciale dag bestaat al.", "Index", "OpeningHours");
 
-            if (uniqueDay.StartDate < DateOnly.FromDateTime(DateTime.Now))
-                return NotifyErrorAndRedirect("De data moet in de toekomst liggen.", "Edit");
+            if (uniqueDay.StartDate <= DateOnly.FromDateTime(DateTime.Now))
+                return NotifyErrorAndRedirect("De data moet in de toekomst liggen.", "Index", "OpeningHours");
 
-            if (!ModelState.IsValid)
-                return NotifyErrorAndRedirect("Er is iets mis gegaan. Mogelijk zijn niet alle velden ingevuld", "Edit");
+            if (!(ModelState.IsValid))
+                return NotifyErrorAndRedirect("Er is iets mis gegaan. Mogelijk zijn niet alle velden ingevuld", "Index", "OpeningHours");
 
             // update to database using transaction
             using var transaction = Context.Database.BeginTransaction();
             try
             {
-                Context.UniqueDays.Update(uniqueDay);
+                var existingDay = Context.UniqueDays.Find(uniqueDay.Id);
+                if (existingDay != null)
+                {
+                    Context.Entry(existingDay).CurrentValues.SetValues(uniqueDay);
+                }
                 Context.SaveChanges();
                 transaction.Commit();
-                NotifyService.Success("De verwachting is bijgewerkt!");
+                NotifyService.Success("De Speciale dag is bijgewerkt.");
             }
             catch (Exception e)
             {
                 Console.WriteLine(e);
                 transaction.Rollback();
-                NotifyService.Error("Er is iets mis gegaan bij het bewerken van de verwachting.");
+                NotifyService.Error("Er is iets mis gegaan bij het bewerken van de Speciale dag.");
             }
 
-            return RedirectToAction("Index","OpeningHours");
+            return RedirectToAction("index","OpeningHours");
         }
 
         [HttpGet]
