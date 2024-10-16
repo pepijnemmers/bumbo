e872846008e10fd6e4e2857f8ac42462522d673f
SemvandenBos <semvandenbos1@gmail.com>
2024-10-17 00:01:04 +0200

    less repetition, proper merge

--- Controllers/PrognosesController.cs
+++ Controllers/PrognosesController.cs
@@ -27,7 +27,7 @@ namespace BumboApp.Controllers
             if (currentPageNumber <= 0) { currentPageNumber = DefaultPage; }
             if (currentPageNumber > maxPages) { currentPageNumber = maxPages; }
 
-            List<WeekPrognosis> prognosesForPage = 
+            List<WeekPrognosis> prognosesForPage =
                 prognoses
                 .Skip((currentPageNumber - 1) * PageSize)
                 .Take(PageSize)
@@ -36,7 +36,7 @@ namespace BumboApp.Controllers
             ViewBag.PageNumber = currentPageNumber;
             ViewBag.PageSize = PageSize;
             ViewBag.MaxPages = maxPages;
-            
+
             ViewBag.ImageUrl = imageUrl;
             ViewBag.OverviewDesc = overviewDesc;
 
@@ -47,14 +47,14 @@ namespace BumboApp.Controllers
             var dateParts = id.Split('-');
             if (dateParts.Length != 3)
             {
-                return HandleInvalidInput("Ongeldige link: dd-MM-yyyy verwacht");
+                return NotifyErrorAndRedirect("Ongeldige link: dd-MM-yyyy verwacht", "Index");
             }
 
             if (!int.TryParse(dateParts[0], out int day) ||
                 !int.TryParse(dateParts[1], out int month) ||
                 !int.TryParse(dateParts[2], out int year))
             {
-                return HandleInvalidInput("Ongeldige link: de datum bevat niet numerieke waarden");
+                return NotifyErrorAndRedirect("Ongeldige link: de datum bevat niet numerieke waarden", "Index");
             }
 
             DateOnly startDate;
@@ -64,7 +64,7 @@ namespace BumboApp.Controllers
             }
             catch (ArgumentOutOfRangeException e)
             {
-                return HandleInvalidInput("Ongeldige link: de datum bestaat niet");
+                return NotifyErrorAndRedirect("Ongeldige link: de datum bestaat niet", "Index");
             }
 
             DayOfWeek dayOfWeek = CultureInfo.InvariantCulture.Calendar.GetDayOfWeek(startDate.ToDateTime(new TimeOnly(0, 0)));
@@ -95,42 +95,36 @@ namespace BumboApp.Controllers
         [HttpPost]
         public IActionResult Update(List<Prognosis> prognoses)
         {
+            //Validation
             if (prognoses == null || !prognoses.Any())
             {
-                return HandleInvalidInput("Er is iets mis gegaan bij het bewerken van de prognose.");
+                return NotifyErrorAndRedirect("Er is iets mis gegaan bij het bewerken van de prognose.", "Index");
             }
-            //foreach (Prognosis prognosis in prognoses)
-            //{
+            foreach (Prognosis prognosis in prognoses)
+            {
+                Prognosis existingPrognosis = Context.Prognoses
+                                        .Single(p => p.Department == prognosis.Department && p.Date == prognosis.Date);
+                if (existingPrognosis == null)
+                {
+                    return NotifyErrorAndRedirect("Er is iets mis gegaan. Mogelijk zijn niet alle velden ingevuld", "Index");
+                }
+                float employees = existingPrognosis.NeededEmployees;
+                float hours = existingPrognosis.NeededHours;
+                if (employees < 0 || hours < 0) //TODO de seeddata voldoet niet aan deze eisen (hours * 8 != employees) :( en floats zijn niet accuraat genoeg? 
+                {
+                    return NotifyErrorAndRedirect("De data is ongeldig", "Index");
+                }
+            }
 
-
-            //    if (existingPrognosis != null)
-            //    {
-            //        existingPrognosis.NeededHours = prognosis.NeededHours;
-            //        existingPrognosis.NeededEmployees = prognosis.NeededEmployees;
-            //    }
-            //}
-
-            using var transaction = _context.Database.BeginTransaction();
+            using var transaction = Context.Database.BeginTransaction();
             try
             {
                 foreach (Prognosis prognosis in prognoses)
                 {
-                    Prognosis existingPrognosis = _context.Prognoses
-                        .Single(p => p.Department == prognosis.Department && p.Date == prognosis.Date);
-                    if (existingPrognosis == null)
-                    {
-                        return HandleInvalidInput("Er is iets mis gegaan. Mogelijk zijn niet alle velden ingevuld");
-                    }
-                    float employees = existingPrognosis.NeededEmployees;
-                    float hours = existingPrognosis.NeededHours;
-                    if (employees < 0 || hours < 0 || hours * 8 != employees) //TODO de seeddata voldoet niet aan deze eisen :( en floats zijn niet accuraat genoeg?
-                    {
-                        return HandleInvalidInput("De data is ongeldig");
-                    }
-                    _context.Prognoses.Update(existingPrognosis);
+                    Context.Prognoses.Update(prognosis);
                 }
 
-                _context.SaveChanges();
+                Context.SaveChanges();
                 transaction.Commit();
                 NotifyService.Success("De prognose is bijgewerkt!");
             }
@@ -140,14 +134,6 @@ namespace BumboApp.Controllers
                 transaction.Rollback();
                 NotifyService.Error("Er is iets mis gegaan bij het bewerken van de prognose.");
             }
-
-            //_context.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        private IActionResult HandleInvalidInput(string errorMessage)
-        {
-            NotifyService.Error(errorMessage);
             return RedirectToAction("Index");
         }
     }
