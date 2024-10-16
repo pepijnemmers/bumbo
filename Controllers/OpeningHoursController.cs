9b081240266a5dd78bd129d85c45966fbd5c3e55
SemvandenBos <semvandenbos1@gmail.com>
2024-10-16 17:00:53 +0200

    Changed to server side validation and added update

--- Controllers/OpeningHoursController.cs
+++ Controllers/OpeningHoursController.cs
@@ -19,7 +19,46 @@ namespace BumboApp.Controllers
         [HttpPost]
         public IActionResult Update(List<OpeningHour> openingHours)
         {
-            return View();
+            //Validation
+            foreach (var openingHour in openingHours)
+            {
+                Console.WriteLine(openingHour.WeekDay + ": " + openingHour.OpeningTime + "-" + openingHour.ClosingTime);
+                if (openingHour.OpeningTime != null && openingHour.ClosingTime != null)
+                {
+                    if (openingHour.OpeningTime > openingHour.ClosingTime)
+                    {
+                        return NotifyErrorAndRedirect("De openingstijd kan niet na de sluitingstijd zijn", "Index");
+                    }
+                }
+                else if (!(openingHour.OpeningTime == null && openingHour.ClosingTime == null))
+                {
+                    return NotifyErrorAndRedirect("De openingstijd en sluitingstijd moeten ingevuld zijn", "Index");
+                }
+            }
+
+
+
+            // update to database using transaction
+            using var transaction = Context.Database.BeginTransaction();
+            try
+            {
+                foreach (var openingHour in openingHours)
+                {
+                    Console.WriteLine(openingHour.WeekDay + ": " + openingHour.OpeningTime + "-" + openingHour.ClosingTime);
+                    Context.OpeningHours.Update(openingHour);
+                }
+                Context.SaveChanges();
+                transaction.Commit();
+                NotifyService.Success("De openingstijden zijn bijgewerkt!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                transaction.Rollback();
+                NotifyService.Error("Er is iets mis gegaan bij het bewerken van de openingstijden.");
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
