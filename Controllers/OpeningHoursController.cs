6695ef9c2423bbb91819c3c4304460b2a65d54a1
SemvandenBos <semvandenbos1@gmail.com>
2024-10-17 17:00:28 +0200

    Merged validation with update for better efficiency

--- Controllers/OpeningHoursController.cs
+++ Controllers/OpeningHoursController.cs
@@ -66,37 +66,30 @@ namespace BumboApp.Controllers
         [HttpPost]
         public IActionResult Update(List<OpeningHour> openingHours)
         {
-            //Validation
-            foreach (var openingHour in openingHours)
-            {
-                if (openingHour.OpeningTime != null && openingHour.ClosingTime != null)
-                {
-                    if (openingHour.OpeningTime >= openingHour.ClosingTime)
-                    {
-                        return NotifyErrorAndRedirect("De openingstijd kan niet na de sluitingstijd zijn", "Index");
-                    }
-                }
-                else if (!(openingHour.OpeningTime == null && openingHour.ClosingTime == null))
-                {
-                    return NotifyErrorAndRedirect("De openingstijd en sluitingstijd moeten ingevuld zijn", "Index");
-                }
-            }
-
-            // update to database using transaction
             using var transaction = Context.Database.BeginTransaction();
             try
             {
                 foreach (var openingHour in openingHours)
                 {
+                    if (openingHour.OpeningTime != null && openingHour.ClosingTime != null)
+                    {
+                        if (openingHour.OpeningTime >= openingHour.ClosingTime)
+                        {
+                            return NotifyErrorAndRedirect("De openingstijd kan niet na de sluitingstijd zijn", "Index");
+                        }
+                    }
+                    else if (!(openingHour.OpeningTime == null && openingHour.ClosingTime == null))
+                    {
+                        return NotifyErrorAndRedirect("De openingstijd en sluitingstijd moeten ingevuld zijn", "Index");
+                    }
                     Context.OpeningHours.Update(openingHour);
                 }
                 Context.SaveChanges();
                 transaction.Commit();
                 NotifyService.Success("De openingstijden zijn bijgewerkt!");
             }
-            catch (Exception e)
+            catch
             {
-                Console.WriteLine(e);
                 transaction.Rollback();
                 NotifyService.Error("Er is iets mis gegaan bij het bewerken van de openingstijden.");
             }
