d2e894254243e19f1dc06c9c380ba1d0d7e39ab7
pepijnemmers <pepijn.emmers@hotmail.com>
2024-10-28 00:37:49 +0100

    updated parameters to logical names

--- Controllers/OpeningHoursController.cs
+++ Controllers/OpeningHoursController.cs
@@ -1,16 +1,17 @@
 ﻿using BumboApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using BumboApp.ViewModels;
+using Microsoft.Data.SqlClient;
 
 namespace BumboApp.Controllers
 {
     public class OpeningHoursController : MainController
     {
-        public IActionResult Index(int? page, bool overviewDesc = false, char? usePassedDates = 'n')
+        public IActionResult Index(int? page, SortOrder? orderBy, bool oldDays = false)
         {
             int currentPageNumber = page ?? DefaultPage;
             List<UniqueDay> uniqueDays;
-            if (usePassedDates == 'n')
+            if (!oldDays)
             {
                 uniqueDays = Context.UniqueDays
                     .Where(u => u.EndDate >= DateOnly.FromDateTime(DateTime.Now))
@@ -26,7 +27,7 @@ namespace BumboApp.Controllers
             }
 
             string imageUrl = "~/img/UpArrow.png";
-            if (!overviewDesc)
+            if (orderBy == SortOrder.Descending)
             {
                 imageUrl = "~/img/DownArrow.png";
                 uniqueDays.Reverse();
@@ -46,8 +47,8 @@ namespace BumboApp.Controllers
             ViewBag.PageSize = PageSize;
             ViewBag.MaxPages = maxPages;
             ViewBag.ImageUrl = imageUrl;
-            ViewBag.OverviewDesc = overviewDesc;
-            ViewBag.UsePassedDates = usePassedDates;
+            ViewBag.OrderBy = orderBy ?? SortOrder.Ascending;
+            ViewBag.OldDays = oldDays;
 
             var OpeningHoursViewModel = new OpeningHoursViewModel
             {
