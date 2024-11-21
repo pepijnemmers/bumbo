9b64189fb6e1f251a56d83f226b706b78d45d0d4
MatthijsCr <msj.cromwijk@student.avans.nl>
2024-11-21 14:56:44 +0100

    eerste opzet, maar kan nog niet verder.

--- Controllers/EmployeesController.cs
+++ Controllers/EmployeesController.cs
@@ -1,12 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BumboApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 
 namespace BumboApp.Controllers
 {
     public class EmployeesController : MainController
     {
-        public IActionResult Index()
+        public IActionResult Index(int? page)
         {
-            return View();
+            int currentPageNumber = page ?? DefaultPage;
+            List<Employee> employees = Context.Employees
+                .ToList();
+
+            int maxPages = (int)(Math.Ceiling((decimal)employees.Count / PageSize));
+            if (currentPageNumber <= 0) { currentPageNumber = DefaultPage; }
+            if (currentPageNumber > maxPages) { currentPageNumber = maxPages; }
+
+            List<Employee> employeesForPage =
+                employees
+                .Skip((currentPageNumber - 1) * PageSize)
+                .Take(PageSize)
+                .ToList();
+
+            ViewBag.PageNumber = currentPageNumber;
+            ViewBag.PageSize = PageSize;
+            ViewBag.MaxPages = maxPages;
+
+            return View(employeesForPage);
         }
         public IActionResult Create()
         {
