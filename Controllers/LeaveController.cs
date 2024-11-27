14604175435ed00ebb66af1728e7d7b56d1c75c1
fvanhastenberg <f.vanhastenberg@student.avans.nl>
2024-11-27 12:45:00 +0100

    start backend implementatie

--- Controllers/LeaveController.cs
+++ Controllers/LeaveController.cs
@@ -1,17 +1,43 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BumboApp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BumboApp.Controllers
 {
     public class LeaveController : MainController
     {
+        Employee LoggedInEmployee; // de Employee die is ingelogd (afgeleid van LoggedInUser)
         public IActionResult Index()
         {
-            return View();
+            return View(LoggedInEmployee);
         }
 
         public IActionResult LeaveRequest()
         {
-            return View();
+            return View(LoggedInEmployee);
+        }
+
+        public IActionResult CreateLeaveRequest(LeaveRequest request)
+        {
+            return NotifyErrorAndRedirect("Fail", "Index"); // test dingetje
+
+            /* uncommenten als de LoggedInEmployee correct werkt
+            using var transaction = Context.Database.BeginTransaction();
+
+            try
+            {
+                Context.LeaveRequests.Add(request);
+
+                Context.SaveChanges();
+                transaction.Commit();
+                LoggedInEmployee.leaveRequests.Add(request);
+                return NotifySuccessAndRedirect("De verlofaanvraag is opgeslagen.", "Index");
+            }
+            catch
+            {
+                transaction.Rollback();
+                return NotifyErrorAndRedirect("Er is iets mis gegaan bij het toevoegen van de verlofaanvraag", "Index");
+            }
+            */
         }
     }
 }
