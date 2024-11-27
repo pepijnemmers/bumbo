785268a52cd18e1c061ca41eebac7c6330e653b2
MatthijsCr <msj.cromwijk@student.avans.nl>
2024-11-27 18:42:44 +0100

    made the 2 scheduling methods(Kassa and rest)

--- Controllers/ScheduleController.cs
+++ Controllers/ScheduleController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 using System.Globalization;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
 
 namespace BumboApp.Controllers
 {
@@ -27,6 +28,7 @@ namespace BumboApp.Controllers
                 for (DateOnly scheduledate = startDate; scheduledate <= endDate; scheduledate.AddDays(1))
                 {
                     ScheduleDepartmentDay(department, scheduledate, startDate);
+                    if(!PrognoseHoursHit(department, scheduledate)) {InsertEmptyShifts(department, scheduledate); }
                 }
             }
             List<Employee> employees = (List<Employee>)Context.Employees
@@ -49,12 +51,18 @@ namespace BumboApp.Controllers
                     });
                 }
             }
+
             try
             {
                 Context.SaveChanges(); //more saves between edits or just in the end?
             }
             catch(Exception e) { return NotifyErrorAndRedirect("er is een probleem opgetreden", "Index"); }
             return RedirectToAction("Index",startDate);
+        }
+
+        private void InsertEmptyShifts(Department department, DateOnly scheduledate)
+        {
+            throw new NotImplementedException();
         }
 
         private void ScheduleDepartmentDay(Department department, DateOnly scheduledate, DateOnly startDate)
@@ -156,12 +164,152 @@ namespace BumboApp.Controllers
             return true;
         }
 
+        //schedules shift if possible
         private void ScheduleShift(Department department, DateOnly scheduledate, Employee employee)
         {
             Availability availability = employee.Availabilities.Where(e => e.Date == scheduledate).FirstOrDefault();
+            OpeningHour openingHour = Context.OpeningHours.Where(e => e.WeekDay == scheduledate.DayOfWeek).FirstOrDefault();
+            int startinghour;
+
+            //checks if employee is available and sets starting time
+            if (availability == null)
+            {
+                startinghour = openingHour.OpeningTime.Value.Hour;
+            }
+            else if ((availability.EndTime - availability.StartTime).Hours == 0)
+            {
+                startinghour = availability.StartTime.Hour;
+                if (availability.StartTime.Minute > 0) { startinghour++; }
+            }
+            else return;
+
+            int maxTimeCAO = getMaxTimeCAO(employee,scheduledate, startinghour);
+            int maxTimePrognose = getMaxTimePrognose(department, scheduledate);
+            int maxTimeContract = getMaxTimeContract(employee);
+            int maxTimeAvailable = (availability.EndTime - availability.StartTime).Hours;
+            List<int> maxhours = new List<int> { maxTimeCAO, maxTimePrognose, maxTimeContract,maxTimeAvailable };
+            maxhours.Sort();
+
+            if (maxhours.First() > 0)
+            {
+                int endTime = maxhours.First() + startinghour;
+                if (endTime > 24) { endTime = 24; } //closing maken of gwn houden
+                if (department == Department.Kassa && endTime > openingHour.ClosingTime.Value.Hour + 1) { return; }
+                Context.Add(
+                    new Shift()
+                    {
+                        Department = department,
+                        Employee = employee,
+                        Start = scheduledate.ToDateTime(new TimeOnly(startinghour, 00, 00)),
+                        End = scheduledate.ToDateTime(new TimeOnly(startinghour + maxhours.First(), 00, 00)),
+                        IsFinal = false
+                    });
+            }
+            return;
         }
 
         private bool ScheduleConcurrentShift(Department department, DateOnly scheduledate, Employee employee)
+        {
+            Availability availability = employee.Availabilities.Where(e => e.Date == scheduledate).FirstOrDefault();
+            int availableFrom;
+            int availableTill;
+            TimeOnly cTime = (TimeOnly)Context.OpeningHours.Where(e => e.WeekDay == scheduledate.DayOfWeek).FirstOrDefault().ClosingTime;
+            int startingHour = getNextEmptySpot(department, scheduledate);
+            int closingHour = cTime.Hour;
+            if (cTime.Minute > 0) { closingHour++; }
+
+
+
+            if(availability != null)
+            {
+                availableFrom = availability.StartTime.Hour;
+                availableTill = availability.EndTime.Hour;
+                if (availability.StartTime.Minute > 0) { availableFrom++; }
+                if (availability.EndTime.Minute > 0) { availableTill++; }
+
+                if (!(startingHour < availableFrom && startingHour > availableTill))
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                availableFrom = 0;
+                availableTill = 24;
+            }
+
+            int maxTimeCAO = getMaxTimeCAO(employee, scheduledate, startingHour);
+            int maxTimePrognose = getMaxTimePrognose(department, scheduledate);
+            int maxTimeContract = getMaxTimeContract(employee);
+            int maxTimeAvailable = availableTill - availableFrom;
+            int maxTimeTillClose = closingHour - startingHour;
+
+            List<int> maxhours = new List<int> { maxTimeCAO, maxTimePrognose, maxTimeContract, maxTimeAvailable, maxTimeTillClose };
+            maxhours.Sort();
+
+            if (maxhours.First() > 0)
+            {
+                int endTime = maxhours.First() + startingHour;
+                Context.Add(
+                    new Shift()
+                    {
+                        Department = department,
+                        Employee = employee,
+                        Start = scheduledate.ToDateTime(new TimeOnly(startingHour, 00, 00)),
+                        End = scheduledate.ToDateTime(new TimeOnly(startingHour + maxhours.First(), 00, 00)),
+                        IsFinal = false
+                    });
+            }
+
+
+        }
+
+        private int getNextEmptySpot(Department department, DateOnly scheduledate)
+        {
+            TimeOnly oTime = (TimeOnly)Context.OpeningHours.Where(e => e.WeekDay == scheduledate.DayOfWeek).FirstOrDefault().OpeningTime;
+            TimeOnly cTime = (TimeOnly)Context.OpeningHours.Where(e => e.WeekDay == scheduledate.DayOfWeek).FirstOrDefault().ClosingTime;
+            int openingHour = oTime.Hour;
+            int closingHour = cTime.Hour;
+            if (cTime.Minute > 0) { closingHour++; }
+            List<Shift> shifts = Context.Shifts
+                    .Where(e => e.Start.Date == scheduledate.ToDateTime(new TimeOnly())
+                    && e.Department == Department.Kassa).OrderBy(e => e.Start.Hour).ToList();
+            Shift previous = null;
+            foreach (Shift shift in shifts)
+            {
+                if (previous == null)
+                {
+                    if (shift.Start.Hour == openingHour)
+                    {
+                        previous = shift;
+                        continue;
+                    }
+                    return openingHour;
+                }
+                if (previous.End.Hour >= shift.Start.Hour)
+                {
+                    previous = shift;
+                    continue;
+                }
+            }
+            if (previous.End.Hour == closingHour)
+            {
+                return closingHour;
+            }
+            return previous.End.Hour;
+        }
+
+        private int getMaxTimeContract(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        private int getMaxTimePrognose(Department department, DateOnly scheduledate)
+        {
+            throw new NotImplementedException();
+        }
+
+        private int getMaxTimeCAO(Employee employee, DateOnly scheduledate, int startinghour)
         {
             throw new NotImplementedException();
         }
