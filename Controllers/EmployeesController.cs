0dc79bd1f36a7edb7187d3c1646b63773d2f6876
SemvandenBos <semvandenbos1@gmail.com>
2024-11-28 15:12:47 +0100

    more changes, not yet done

--- Controllers/EmployeesController.cs
+++ Controllers/EmployeesController.cs
@@ -3,12 +3,13 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json.Linq;
+using System.Text.Json;
 
 namespace BumboApp.Controllers
 {
     public class EmployeesController : MainController
     {
-        static readonly HttpClient client = new HttpClient(); //TODO DIT MOET HIER WEG TEST
+        static readonly HttpClient client = new HttpClient(); //TODO DIT MOET HIER WEG
 
 
         public IActionResult Index(int? page)
@@ -44,9 +45,9 @@ namespace BumboApp.Controllers
         }
         public async Task<IActionResult> Details(int id)
         {
-
             var employee = Context.Employees
                 .Include(e => e.User)
+                .Include(e => e.leaveRequests)
                 .SingleOrDefault(e => e.EmployeeNumber == id);
 
             if (employee == null)
@@ -54,24 +55,37 @@ namespace BumboApp.Controllers
                 return NotifyErrorAndRedirect("Werknemer niet gevonden", nameof(Index));
             }
 
-            var apiURL = $"https://json.api-postcode.nl?postcode={employee.Zipcode}&number={employee.HouseNumber}";
 
-            var allEnvVars = Environment.GetEnvironmentVariables();
-            foreach (var key in allEnvVars.Keys)
-            {
-                Console.WriteLine($"{key}: {allEnvVars[key]}");
-            }
+            ViewData["ContractHourUsed"] = "";
+            ViewData["LeaveHourUsed"] = employee.leaveRequests.Where(lr => lr.Status == Status.Geaccepteerd && lr.StartDate.Year.Equals(DateTime.Now.Year));
 
             //var apiKey = Environment.GetEnvironmentVariable("POSTCODE_API_KEY");
+            var apiKey = "669869e4-b372-430b-9b87-d41c72fcfc91";
 
             //if (string.IsNullOrEmpty(apiKey))
             //{
             //    return BadRequest("API key not configured.");
             //}
 
-            client.DefaultRequestHeaders.Clear();
-            //client.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiKey}");
+            var apiURL = $"https://json.api-postcode.nl?postcode={employee.Zipcode}&number={employee.HouseNumber}";
 
+            client.DefaultRequestHeaders.Clear();
+            client.DefaultRequestHeaders.Add("token", apiKey);
+
+            var response = await client.GetAsync(apiURL);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonResponse = await response.Content.ReadAsStringAsync();
+
+                using (JsonDocument doc = JsonDocument.Parse(jsonResponse))
+                {
+                    JsonElement root = doc.RootElement;
+
+                    ViewData["StreetName"] = root.GetProperty("street").GetString();
+                    ViewData["CityName"] = root.GetProperty("city").GetString();
+                }
+            }
 
             return View(employee);
         }
