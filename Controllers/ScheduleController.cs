4fdff68d29d8329229edbedf37eae3cd62c97e08
pepijnemmers <pepijn.emmers@hotmail.com>
2024-11-28 22:34:17 +0100

    WIP start of dayview

--- Controllers/ScheduleController.cs
+++ Controllers/ScheduleController.cs
@@ -7,18 +7,61 @@ namespace BumboApp.Controllers
 {
     public class ScheduleController : MainController
     {
-        public IActionResult Index(string? employeeNumber, DateOnly? startDate, bool? dayView)
+        public IActionResult Index(string? employeeNumber, DateOnly? startDate, bool dayView = true)
         {
             var role = Role.Manager; // TODO: Get role from user
             
-            var selectedEmployee = employeeNumber != null && int.TryParse(employeeNumber, out _) ? Context.Employees.Find(Int32.Parse(employeeNumber)) : null;
-            var isDayView = selectedEmployee != null && (dayView ?? true);
+            var selectedEmployee = employeeNumber != null && int.TryParse(employeeNumber, out _) ? Context.Employees.Find(int.Parse(employeeNumber)) : null;
+            var isDayView = selectedEmployee == null || dayView;
             
             var selectedStartDate = isDayView ? startDate ?? DateOnly.FromDateTime(DateTime.Today) : GetMondayOfWeek(startDate ?? DateOnly.FromDateTime(DateTime.Today));
             var weekNumber = ISOWeek.GetWeekOfYear(selectedStartDate.ToDateTime(new TimeOnly(12, 00)));
             
-            var shifts = new List<Shift>(); // TODO: Get shifts from database
             var employees = Context.Employees.ToList();
+            // var shifts = new List<Shift>(); // TODO: Get shifts from database
+            var shifts = new List<Shift>
+            {
+                new Shift()
+                {
+                    Id = 1,
+                    Department = Department.Vers,
+                    Employee = employees[0],
+                    Start = new DateTime(new DateOnly(2024, 11, 28), new TimeOnly(8, 0)),
+                    End = new DateTime(new DateOnly(2024, 11, 28), new TimeOnly(16, 0)),
+                    IsFinal = true,
+                    ShiftTakeOver = new ShiftTakeOver()
+                },
+                new Shift()
+                {
+                    Id = 2,
+                    Department = Department.Vakkenvullen,
+                    Employee = employees[1],
+                    Start = new DateTime(new DateOnly(2024, 11, 28), new TimeOnly(16, 0)),
+                    End = new DateTime(new DateOnly(2024, 11, 28), new TimeOnly(23, 0)),
+                    IsFinal = true,
+                    ShiftTakeOver = new ShiftTakeOver()
+                },
+                new Shift()
+                {
+                    Id = 3,
+                    Department = Department.Kassa,
+                    Employee = employees[2],
+                    Start = new DateTime(new DateOnly(2024, 11, 29), new TimeOnly(8, 0)),
+                    End = new DateTime(new DateOnly(2024, 11, 29), new TimeOnly(10, 0)),
+                    IsFinal = true,
+                    ShiftTakeOver = new ShiftTakeOver()
+                },
+                new Shift()
+                {
+                    Id = 4,
+                    Department = Department.Kassa,
+                    Employee = employees[2],
+                    Start = new DateTime(new DateOnly(2024, 11, 29), new TimeOnly(8, 0)),
+                    End = new DateTime(new DateOnly(2024, 11, 29), new TimeOnly(16, 0)),
+                    IsFinal = true,
+                    ShiftTakeOver = new ShiftTakeOver()
+                },
+            };
                 
             var viewIsConcept = shifts.Any(shift => !shift.IsFinal);
             
