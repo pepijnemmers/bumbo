32aaa807ca4bb03b7bdb414b220364ef4d7b4fee
Ikkie <ikkie857@outlook.com>
2024-11-28 19:29:51 +0100

    Begin gemaakt aan dashboard. Te beoordelen overnames en verlof zijn te zien.

--- Controllers/DashboardController.cs
+++ Controllers/DashboardController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BumboApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BumboApp.Controllers
 {
@@ -6,6 +8,20 @@ namespace BumboApp.Controllers
     {
         public IActionResult Index()
         {
+            // Retrieve ShiftTakeOvers where the status indicates they need assessment
+            var shiftTakeOvers = Context.ShiftTakeOvers
+                .Include(sto => sto.Shift)           // Include related shift data
+                .Where(sto => sto.Status == Status.Aangevraagd) // Filter by status
+                .ToList();
+
+            var leaveRequests = Context.LeaveRequests
+                .Include(lr => lr.Employee)
+                .Where(lr => lr.Status == Status.Aangevraagd)
+                .ToList();
+
+            ViewBag.ShiftTakeOvers = shiftTakeOvers;
+            ViewBag.LeaveRequests = leaveRequests;
+
             return View();
         }
     }
