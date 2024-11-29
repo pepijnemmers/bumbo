0285de7a89f2ee40b1160142ce62fbd4b2c4b46a
SemvandenBos <semvandenbos1@gmail.com>
2024-11-29 08:54:06 +0100

    mostly functional but no nice view

--- Controllers/AvailabilityController.cs
+++ Controllers/AvailabilityController.cs
@@ -1,12 +1,58 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BumboApp.Models;
+using BumboApp.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace BumboApp.Controllers
 {
     public class AvailabilityController : MainController
     {
-        public IActionResult Index()
+        public IActionResult Index(string? id)
         {
-            return View();
+            if (id == null)
+            {
+                DayOfWeek dayOfWeek = CultureInfo.InvariantCulture.Calendar.GetDayOfWeek(DateTime.Now);
+                string newId = DateTime.Now.AddDays(-(int)dayOfWeek + 1).ToString("dd-MM-yyyy");
+                return RedirectToAction("Index", new { id = newId });
+            }
+
+
+            var dateParts = id.Split('-');
+            if (dateParts.Length != 3)
+            {
+                return NotifyErrorAndRedirect("Ongeldige link: dd-MM-yyyy verwacht", "Index", "Dashboard");
+            }
+
+            if (!int.TryParse(dateParts[0], out int day) ||
+                !int.TryParse(dateParts[1], out int month) ||
+                !int.TryParse(dateParts[2], out int year))
+            {
+                return NotifyErrorAndRedirect("Ongeldige link: de datum bevat niet numerieke waarden", "Index", "Dashboard");
+            }
+
+            DateOnly startDate;
+            try
+            {
+                startDate = new DateOnly(year, month, day);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                return NotifyErrorAndRedirect("Ongeldige link: de datum bestaat niet", "Index", "Dashboard");
+            }
+
+            List<Availability> availabilityList = Context.Availabilities.Where(a => a.EmployeeNumber == 1 && a.Date >= startDate && a.Date < startDate.AddDays(7)).ToList(); //Hardcoded employeeNumber
+
+            List<SchoolSchedule> schoolScheduleList = Context.SchoolSchedules.Where(a => a.EmployeeNumber == 1 && a.Date >= startDate && a.Date < startDate.AddDays(7)).ToList(); //Hardcoded employeeNumber
+
+            var viewModel = new AvailabilityViewModel
+            {
+                WeekNr = CultureInfo.InvariantCulture.Calendar.GetWeekOfYear(startDate.AddDays(3).ToDateTime(new TimeOnly(0, 0)), CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday),
+                StartDate = startDate,
+                availabilityList = availabilityList,
+                schoolScheduleList = schoolScheduleList
+            };
+            return View(viewModel);
         }
 
         public IActionResult Update()
---
+++ ViewModels/AvailabilityViewModel.cs
@@ -0,0 +1,12 @@
+﻿using BumboApp.Models;
+
+namespace BumboApp.ViewModels
+{
+    public class AvailabilityViewModel
+    {
+        public int WeekNr { get; set; }
+        public DateOnly StartDate { get; set; }
+        public List<Availability> availabilityList { get; set; } = new List<Availability>();
+        public List<SchoolSchedule> schoolScheduleList { get; set; } = new List<SchoolSchedule>();
+    }
+}
