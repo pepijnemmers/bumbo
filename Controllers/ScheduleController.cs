60c97029d4c411377b1f28b379295c8dda7044a9
pepijnemmers <pepijn.emmers@hotmail.com>
2024-11-29 11:49:02 +0100

    WIP get shifts for weekview

--- Controllers/ScheduleController.cs
+++ Controllers/ScheduleController.cs
@@ -13,16 +13,17 @@ namespace BumboApp.Controllers
             var role = LoggedInUserRole;
             
             var selectedEmployee = employeeNumber != null && int.TryParse(employeeNumber, out _) ? Context.Employees.Find(int.Parse(employeeNumber)) : null;
-            var isDayView = selectedEmployee == null || dayView;
+            bool isDayView = selectedEmployee == null || dayView;
             
             var selectedStartDate = isDayView ? startDate ?? DateOnly.FromDateTime(DateTime.Today) : GetMondayOfWeek(startDate ?? DateOnly.FromDateTime(DateTime.Today));
-            var weekNumber = ISOWeek.GetWeekOfYear(selectedStartDate.ToDateTime(new TimeOnly(12, 00)));
+            int weekNumber = ISOWeek.GetWeekOfYear(selectedStartDate.ToDateTime(new TimeOnly(12, 00)));
             
             var employees = Context.Employees.ToList();
-            var shifts = new List<Shift>(); // TODO: Get shifts from database > for day view of all employees, order by department
-
+            List<Shift> shifts;
+            
             if (isDayView)
             {
+                // dayview
                 if (selectedEmployee != null)
                 {
                     shifts = Context.Shifts
@@ -41,7 +42,24 @@ namespace BumboApp.Controllers
             }
             else
             {
-                
+                // weekview
+                if (selectedEmployee != null)
+                {
+                    shifts = Context.Shifts
+                        .Where(shift => shift.Employee == selectedEmployee 
+                                        && DateOnly.FromDateTime(shift.Start) >= selectedStartDate
+                                        && DateOnly.FromDateTime(shift.Start) < selectedStartDate.AddDays(7))
+                        .OrderBy(shift => shift.Department)
+                        .ToList();
+                }
+                else
+                {
+                    shifts = Context.Shifts
+                        .Where(shift => DateOnly.FromDateTime(shift.Start) >= selectedStartDate
+                                        && DateOnly.FromDateTime(shift.Start) < selectedStartDate.AddDays(7))
+                        .OrderBy(shift => shift.Department)
+                        .ToList();
+                }
             }
             
             var viewIsConcept = shifts.Any(shift => !shift.IsFinal);
