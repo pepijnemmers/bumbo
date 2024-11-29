e05846c5a23c0b4b3b5008d61c3f9795ab2fc430
pepijnemmers <pepijn.emmers@hotmail.com>
2024-11-29 16:52:03 +0100

    code clean up

--- Controllers/ScheduleController.cs
+++ Controllers/ScheduleController.cs
@@ -8,59 +8,26 @@ namespace BumboApp.Controllers
 {
     public class ScheduleController : MainController
     {
-        public IActionResult Index(string? employeeNumber, DateOnly? startDate, bool dayView = true)
+        public IActionResult Index(string? employeeNumber, string? startDate, bool dayView = true)
         {
             var role = LoggedInUserRole;
             
             var selectedEmployee = employeeNumber != null && int.TryParse(employeeNumber, out _) ? Context.Employees.Find(int.Parse(employeeNumber)) : null;
             bool isDayView = selectedEmployee == null || dayView;
             
-            var selectedStartDate = isDayView ? startDate ?? DateOnly.FromDateTime(DateTime.Today) : GetMondayOfWeek(startDate ?? DateOnly.FromDateTime(DateTime.Today));
+            var selectedStartDate = startDate != null 
+                ? (isDayView ? DateOnly.FromDateTime(DateTime.Parse(startDate)) : GetMondayOfWeek(DateOnly.FromDateTime(DateTime.Parse(startDate))))
+                : (isDayView ? DateOnly.FromDateTime(DateTime.Today) : GetMondayOfWeek(DateOnly.FromDateTime(DateTime.Today)));
+            
             int weekNumber = ISOWeek.GetWeekOfYear(selectedStartDate.ToDateTime(new TimeOnly(12, 00)));
             
             var employees = Context.Employees.ToList();
-            List<Shift> shifts;
-            
-            if (isDayView)
-            {
-                // dayview
-                if (selectedEmployee != null)
-                {
-                    shifts = Context.Shifts
-                        .Where(shift => shift.Employee == selectedEmployee 
-                                        && DateOnly.FromDateTime(shift.Start) == selectedStartDate)
-                        .OrderBy(shift => shift.Department)
-                        .ToList();
-                }
-                else
-                {
-                    shifts = Context.Shifts
-                        .Where(shift => DateOnly.FromDateTime(shift.Start) == selectedStartDate)
-                        .OrderBy(shift => shift.Department)
-                        .ToList();
-                }
-            }
-            else
-            {
-                // weekview
-                if (selectedEmployee != null)
-                {
-                    shifts = Context.Shifts
-                        .Where(shift => shift.Employee == selectedEmployee 
-                                        && DateOnly.FromDateTime(shift.Start) >= selectedStartDate
-                                        && DateOnly.FromDateTime(shift.Start) < selectedStartDate.AddDays(7))
-                        .OrderBy(shift => shift.Department)
-                        .ToList();
-                }
-                else
-                {
-                    shifts = Context.Shifts
-                        .Where(shift => DateOnly.FromDateTime(shift.Start) >= selectedStartDate
-                                        && DateOnly.FromDateTime(shift.Start) < selectedStartDate.AddDays(7))
-                        .OrderBy(shift => shift.Department)
-                        .ToList();
-                }
-            }
+            var shifts = Context.Shifts
+                .Where(shift => isDayView
+                    ? DateOnly.FromDateTime(shift.Start) == selectedStartDate && (selectedEmployee == null || shift.Employee == selectedEmployee)
+                    : DateOnly.FromDateTime(shift.Start) >= selectedStartDate && DateOnly.FromDateTime(shift.Start) < selectedStartDate.AddDays(7) && (selectedEmployee == null || shift.Employee == selectedEmployee))
+                .OrderBy(shift => shift.Department)
+                .ToList();
             
             var viewIsConcept = shifts.Any(shift => !shift.IsFinal);
             
