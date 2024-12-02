7c0e7de81ba19f14a87899640c2514f4c2986cc1
pepijnemmers <pepijn.emmers@hotmail.com>
2024-12-02 17:30:30 +0100

    Schedule improved for employee view

--- Controllers/ScheduleController.cs
+++ Controllers/ScheduleController.cs
@@ -10,30 +10,47 @@ namespace BumboApp.Controllers
     {
         public IActionResult Index(string? employeeNumber, string? startDate, bool dayView = true)
         {
-            var role = LoggedInUserRole;
+            // Selected employee filter or logged in employee
+            Employee? selectedEmployee = null;
+            if (LoggedInUserRole == Role.Employee)
+            {
+                selectedEmployee = Context.Employees.FirstOrDefault(employee => employee.User.Id == LoggedInUserId);
+            }
+            else
+            {
+                selectedEmployee = employeeNumber != null && int.TryParse(employeeNumber, out _) ? Context.Employees.Find(int.Parse(employeeNumber)) : null;
+            }
             
-            var selectedEmployee = employeeNumber != null && int.TryParse(employeeNumber, out _) ? Context.Employees.Find(int.Parse(employeeNumber)) : null;
+            // Day or week view and selected start date
             bool isDayView = selectedEmployee == null || dayView;
-            
             var selectedStartDate = startDate != null 
                 ? (isDayView ? DateOnly.FromDateTime(DateTime.Parse(startDate)) : GetMondayOfWeek(DateOnly.FromDateTime(DateTime.Parse(startDate))))
                 : (isDayView ? DateOnly.FromDateTime(DateTime.Today) : GetMondayOfWeek(DateOnly.FromDateTime(DateTime.Today)));
-            
             int weekNumber = ISOWeek.GetWeekOfYear(selectedStartDate.ToDateTime(new TimeOnly(12, 00)));
             
+            // Get all employees for filter
             var employees = Context.Employees.ToList();
+            
+            // Get all shifts for selected date (based on day/week view) and selected/loggedin employee
             var shifts = Context.Shifts
                 .Where(shift => isDayView
-                    ? DateOnly.FromDateTime(shift.Start) == selectedStartDate && (selectedEmployee == null || shift.Employee == selectedEmployee)
-                    : DateOnly.FromDateTime(shift.Start) >= selectedStartDate && DateOnly.FromDateTime(shift.Start) < selectedStartDate.AddDays(7) && (selectedEmployee == null || shift.Employee == selectedEmployee))
+                    ? DateOnly.FromDateTime(shift.Start) == selectedStartDate 
+                      && (selectedEmployee == null || shift.Employee == selectedEmployee) 
+                      && (LoggedInUserRole != Role.Employee || shift.IsFinal)
+                    : DateOnly.FromDateTime(shift.Start) >= selectedStartDate 
+                      && DateOnly.FromDateTime(shift.Start) < selectedStartDate.AddDays(7) 
+                      && (selectedEmployee == null || shift.Employee == selectedEmployee) 
+                      && (LoggedInUserRole != Role.Employee || shift.IsFinal))
                 .OrderBy(shift => shift.Department)
                 .ToList();
             
+            // Check if view is concept or final
             var viewIsConcept = shifts.Any(shift => !shift.IsFinal);
             
+            // Create view model and return view
             var viewModel = new ScheduleViewModel()
             {
-                Role = role,
+                Role = LoggedInUserRole,
                 ViewIsConcept = viewIsConcept,
                 SelectedStartDate = selectedStartDate,
                 WeekNumber = weekNumber,
