009dfeb8394a2bda902d37237193b3f0800cb7cc
fvanhastenberg <167075666+fvanhastenberg@users.noreply.github.com>
2024-12-04 11:22:45 +0100

    Verlof aanvragen, beoordelen en ziekmelden (#58)
    * placeholder bestaande verlofaanvraag + begin card voor verlofaanvraag
    
    * start backend implementatie
    
    * loading leave requests from DB and made all LeaveRequest fields mandatory
    
    * voorlopige rolfixes
    
    * db toevoeging compleet + opschoningen code
    
    * code cleanup & view styling
    
    * pagination and sortorder added
    
    * subtracting leave hours from the employee
    
    * added notification to manager
    
    * Revert "added notification to manager"
    
    This reverts commit 416befb174ceb7f76fde8966709b36dcb3dd0772.
    
    * extra validation check en minimum datum naar morgen
    
    * bugfixes
    
    * added responsiveness
    
    * aanpassingen op nieuwe inlog
    
    * added verification for overlapping leaverequests
    
    * set loggedInEmployee to private
    
    * user role changes made & leaverequest to datetime
    
    * correct amountOfLeaveHours & view changes
    
    * added Status filter
    
    * basis frontend ziekmelden
    
    * bugfix
    
    * removed some comments
    
    * modal for employee working
    
    * empty state change
    
    * amountOfLeaveHours gets subtracted & user sees their leftover LeaveHours
    
    * responsive fixes
    
    * frontend fixes
    
    * backend added (not fully tested)
    
    * extra validation added & pagination added
    
    * basis verlof-beoordelen
    
    * temp change
    
    * manager notification fixed
    
    * db en pagination fixes
    
    * text fix
    
    * database entry status leaverequest, code cleanup, small frontend changes
    
    * alleen leaveRequests met einddatum in de toekomst te zien
    
    * LeaveHours per year correctly working
    
    * code cleanup & code expandable
    
    * reverted css changes
    
    * small view title fix
    
    * KeyNotFoundException bugfix

---
+++ ViewModels/LeaveRequestViewModel.cs
@@ -0,0 +1,16 @@
+﻿using BumboApp.Models;
+
+namespace BumboApp.ViewModels
+{
+    public class LeaveRequestViewModel
+    {
+        public required List<LeaveRequest> LeaveRequestsForPage { get; set; }
+        public required Status? SelectedStatus { get; set; }
+
+        public required List<SickLeave> SickLeaves { get; set; }
+
+        public required Employee LoggedInEmployee { get; set; }
+
+        public required List<Employee> AllEmployees { get; set; }
+    }
+}
