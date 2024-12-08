656411a688156d4200c452d825a68d9202cd71f6
pepijnemmers <pepijn.emmers@hotmail.com>
2024-12-08 23:19:02 +0100

    moved js to correct place to reduce errors

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -11,38 +11,6 @@ document.getElementById('mobile-close-menu').addEventListener('click', function 
     document.getElementById('aside-nav-primary').classList.add('d-none');
 });
 
-//Prognoses details
-document.getElementById('prognosisEditButton').addEventListener('click', function () {
-    var form = document.getElementById("form");
-    var overview = document.getElementById("overview");
-    var prognosisEditButton = document.getElementById("prognosisEditButton");
-
-    if (form.style.display === "none") {
-        overview.style.display = "none";
-        form.style.display = "block";
-        prognosisEditButton.textContent = "annuleren";
-    } else {
-        form.style.display = "none";
-        overview.style.display = "flex";
-        prognosisEditButton.textContent = "bewerken";
-    }
-});
-
-function updateHours(id) {
-    let employeesInput = document.getElementById(id + '-employees');
-    let hoursInput = document.getElementById(id + '-hours');
-
-    let employees = employeesInput.value;
-    hoursInput.value = employees * 8;
-}
-
-function updateEmployees(id) {
-    let hoursInput = document.getElementById(id + '-hours');
-    let employeesInput = document.getElementById(id + '-employees');
-    let hours = hoursInput.value;
-    employeesInput.value = hours / 8;
-}
-
 function checkDate() {
     let startDateField = document.getElementById("startDate");
     let endDateField = document.getElementById("endDate");
@@ -51,21 +19,20 @@ function checkDate() {
 
     let date = new Date(startDateField.value);
     let dayNumber = date.getDay();
-    document.getElementById("prognosisCreateButton").disabled = (dayNumber != 1 || date < currentDate);
+    document.getElementById("prognosisCreateButton").disabled = (dayNumber !== 1 || date < currentDate);
 
-    if (dayNumber != 1) {
+    if (dayNumber !== 1) {
         warningMessage.classList.remove("d-none");
     } else {
         warningMessage.classList.add("d-none");
     }
 
-    if (!(dayNumber != 1 || date < currentDate)) {
+    if (!(dayNumber !== 1 || date < currentDate)) {
         date.setDate(date.getDate() + 6);
-        let formattedDate = ('0' + date.getDate()).slice(-2) + '-' +
+        //formatten voor dd-mm-yyyy (met 07 ipv 7)
+        endDateField.textContent = ('0' + date.getDate()).slice(-2) + '-' +
             ('0' + (date.getMonth() + 1)).slice(-2) + '-' +
-            date.getFullYear(); //formatten voor dd-mm-yyyy (met 07 ipv 7)
-
-        endDateField.textContent = formattedDate;
+            date.getFullYear();
     }
     else {
         endDateField.textContent = 'dd-mm-yyyy';
