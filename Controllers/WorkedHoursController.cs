a27d8f9f008ed2ca80b1091c8f4868aee59991c8
pepijnemmers <pepijn.emmers@hotmail.com>
2025-01-06 11:58:07 +0100

    added new controllers and views

---
+++ Controllers/WorkedHoursController.cs
@@ -0,0 +1,26 @@
+using BumboApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace BumboApp.Controllers;
+
+public class WorkedHoursController : MainController
+{
+    public override void OnActionExecuting(ActionExecutingContext context)
+    {
+        base.OnActionExecuting(context);
+        CheckPageAccess(Role.Manager);
+    }
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    }
+    
+    [HttpGet]
+    public IActionResult Update()
+    {
+        return View();
+    }
+}
