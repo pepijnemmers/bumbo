d8f80ecaaa2d2e2dcf8a9cc8e28b3c3294db16b2
SemvandenBos <semvandenbos1@gmail.com>
2025-01-09 00:29:36 +0100

    Eerste werkende versie toeslag

--- Controllers/HoursExportController.cs
+++ Controllers/HoursExportController.cs
@@ -1,8 +1,10 @@
+using BumboApp.Helpers;
 using BumboApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.EntityFrameworkCore;
 using OfficeOpenXml;
+using System.Numerics;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace BumboApp.Controllers;
@@ -16,48 +18,197 @@ public class HoursExportController : MainController
     }
 
     [HttpGet]
-    public IActionResult Index()
+    public IActionResult Index(int? month, int? year)
     {
+        ViewBag.Month = month;
+        ViewBag.Year = year;
         return View();
+    }
+
+    [HttpPost]
+    public IActionResult CanDownload(int month, int year)
+    {
+        List<WorkedHour> workedHours = Context.WorkedHours.Where(wh => wh.DateOnly.Year == year && wh.DateOnly.Month == month).Include(wh => wh.Employee).ToList();
+
+        if (workedHours == null || !workedHours.Any())
+        {
+            NotifyService.Error("Er is geen data van deze periode");
+            return RedirectToAction(nameof(Index), new { month, year });
+        }
+
+        return Download(month, year);
     }
 
     [HttpPost]
     public IActionResult Download(int month, int year)
     {
-        List<WorkedHour> workedHours = Context.WorkedHours.Where(wh => wh.DateOnly.Year == year && wh.DateOnly.Month == month).Include(wh => wh.Employee).ToList();
-        List<string> dataList = new List<string>();
+        List<WorkedHour> workedHours = Context.WorkedHours.Where(wh => wh.DateOnly.Year == year && wh.DateOnly.Month == month)
+            .Include(wh => wh.Employee)
+            .Include(wh => wh.Breaks).ToList();
 
+        List<string> dataList = new List<string>();
+        bool isConcept = false;
         foreach (var workedHour in workedHours)
         {
-            dataList.Add(workedHour.Employee.UserId + "," + workedHour.Employee.FirstName + "," + (workedHour.EndTime - workedHour.StartTime) + "," + "TOESLAG");
-            Console.WriteLine(workedHour.Employee.UserId + "," + workedHour.Employee.FirstName + "," + (workedHour.EndTime - workedHour.StartTime) + "," + "TOESLAG");
+            if (workedHour.EndTime == null)
+            {
+                continue;
+            }
+
+            if (workedHour.Status.Equals(HourStatus.Concept))
+            {
+                isConcept = true;
+            }
+
+            string userId = workedHour.Employee.UserId;
+            string name = workedHour.Employee.FirstName;
+            string workedTime = (workedHour.EndTime - workedHour.StartTime).ToString();
+            double bonusPercent = Math.Round((CalculateBonus(workedHour) * 100) - 100, 1);
+
+            string dataString = $"{userId};{name};{workedTime};{bonusPercent}%";
+            dataList.Add(dataString);
         }
-        return RedirectToAction(nameof(Index));
+
+        var excelStream = GenerateExcel(dataList);
+        string fileName = $"gewerkteUren-{month}-{year}" + (isConcept ? "-CONCEPT" : "") + ".xlsx";
+        return File(excelStream.ToArray(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", fileName);
     }
 
-    private double CalculateBonus()
+    private double CalculateBonus(WorkedHour workedHour)
     {
+        var intervals = GetWorkedIntervals(workedHour);
+        var bonusPeriods = GetBonusPeriods(workedHour.DateOnly.DayNumber);
 
-        return 0;
+        double totalWorkedTime = 0;
+        double totalWeightedBonus = 0;
+        foreach (var interval in intervals)
+        {
+            totalWorkedTime += (interval.End - interval.Start).TotalSeconds;
+            foreach (var bonusPeriod in bonusPeriods)
+            {
+                totalWeightedBonus += CalculateWeightedTime(interval, bonusPeriod);
+            }
+        }
+        return totalWeightedBonus / totalWorkedTime;
     }
 
-    private MemoryStream GenerateExcel(List<string> inputData)
+    private List<(TimeOnly Start, TimeOnly End)> GetWorkedIntervals(WorkedHour workedHour)
+    {
+        List<Break> breaks = workedHour.Breaks ?? new();
+        var intervals = new List<(TimeOnly Start, TimeOnly End)>();
+        TimeOnly currentStart = workedHour.StartTime;
+
+        foreach (var brk in breaks.OrderBy(b => b.StartTime))
+        {
+            if (brk != null && brk.EndTime.HasValue)
+            {
+                intervals.Add((currentStart, brk.StartTime));
+                currentStart = brk.EndTime.Value;
+            }
+        }
+
+        // Add the remaining time after the last break
+        if (currentStart < workedHour.EndTime)
+        {
+            intervals.Add((currentStart, workedHour.EndTime.Value));
+        }
+
+        return intervals;
+    }
+
+    //calculate the amount of seconds*factor of a period of work and a bonus period
+    private double CalculateWeightedTime((TimeOnly Start, TimeOnly End) period, BonusPeriod bonusPeriod)
+    {
+        if (period.Start >= bonusPeriod.EndTime || period.End <= bonusPeriod.StartTime)
+        {
+            return 0;
+        }
+        var overlapStart = Math.Max(period.Start.ToTimeSpan().TotalSeconds, bonusPeriod.StartTime.ToTimeSpan().TotalSeconds);
+        var overlapEnd = Math.Min(period.End.ToTimeSpan().TotalSeconds, bonusPeriod.EndTime.ToTimeSpan().TotalSeconds);
+
+        //Console.WriteLine(overlapStart + "-" + overlapEnd + "*" + bonusPeriod.Multiplier
+        return (overlapEnd - overlapStart) * bonusPeriod.Multiplier;
+    }
+
+    //TODO uit json halen? dit hangt af van het CAO
+    private List<BonusPeriod> GetBonusPeriods(int dayNumber)
+    {
+        var result = new List<BonusPeriod>();
+        switch (dayNumber)
+        {
+            case 0:
+                result.Add(new BonusPeriod
+                {
+                    StartTime = new TimeOnly(0, 0),
+                    EndTime = new TimeOnly(23, 59),
+                    Multiplier = 2d
+                });
+                break;
+            case 6:
+                result.Add(new BonusPeriod
+                {
+                    StartTime = new TimeOnly(0, 0),
+                    EndTime = new TimeOnly(6, 00),
+                    Multiplier = 1.5d
+                });
+                result.Add(new BonusPeriod
+                {
+                    StartTime = new TimeOnly(6, 0),
+                    EndTime = new TimeOnly(18, 00),
+                    Multiplier = 1d
+                });
+                result.Add(new BonusPeriod
+                {
+                    StartTime = new TimeOnly(18, 0),
+                    EndTime = new TimeOnly(23, 59),
+                    Multiplier = 1.5d
+                });
+                break;
+            default:
+                result.Add(new BonusPeriod
+                {
+                    StartTime = new TimeOnly(0, 0),
+                    EndTime = new TimeOnly(6, 00),
+                    Multiplier = 1.5d
+                });
+                result.Add(new BonusPeriod
+                {
+                    StartTime = new TimeOnly(6, 0),
+                    EndTime = new TimeOnly(20, 00),
+                    Multiplier = 1d
+                });
+                result.Add(new BonusPeriod
+                {
+                    StartTime = new TimeOnly(20, 0),
+                    EndTime = new TimeOnly(21, 0),
+                    Multiplier = 4d / 3d
+                });
+                result.Add(new BonusPeriod
+                {
+                    StartTime = new TimeOnly(21, 0),
+                    EndTime = new TimeOnly(23, 59),
+                    Multiplier = 1.5d
+                });
+                break;
+        }
+        return result;
+    }
+
+    private static MemoryStream GenerateExcel(List<string> inputData)
     {
         ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
         using (var package = new ExcelPackage())
         {
             var worksheet = package.Workbook.Worksheets.Add("Gewerkte uren");
-            //HEADERS (onnodig ofniet?)
-            //worksheet.Cells[1, 1].Value = "BID";
-            //worksheet.Cells[1, 2].Value = "Naam";
-            //worksheet.Cells[1, 3].Value = "Gewerkte uren";
-            //worksheet.Cells[1, 4].Value = "Toeslag";
+            worksheet.Cells[1, 1].Value = "BID";
+            worksheet.Cells[1, 2].Value = "Naam";
+            worksheet.Cells[1, 3].Value = "Gewerkte uren";
+            worksheet.Cells[1, 4].Value = "Toeslag";
 
-
-            int row = 1;
+            int row = 2;
             foreach (var line in inputData)
             {
-                string[] columns = line.Split(',');
+                string[] columns = line.Split(';');
                 for (int col = 0; col < columns.Length; col++)
                 {
                     worksheet.Cells[row, col + 1].Value = columns[col];
