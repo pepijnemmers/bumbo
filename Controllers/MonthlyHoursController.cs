3afdfbba147db773fa0b439b13a09bff7f6d222c
fvanhastenberg <f.vanhastenberg@student.avans.nl>
2025-01-09 18:48:12 +0100

    ViewModel created & updated view values so they are no longer hard coded

--- Controllers/MonthlyHoursController.cs
+++ Controllers/MonthlyHoursController.cs
@@ -1,4 +1,5 @@
 using BumboApp.Models;
+using BumboApp.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
@@ -13,9 +14,32 @@ public class MonthlyHoursController : MainController
     }
 
     [HttpGet]
-    public IActionResult Index()
+    public IActionResult Index(int? selectedMonth, int? selectedYear)
     {
-        return View();
+        if (selectedMonth > 11 || selectedMonth == null)
+        {
+            selectedMonth = DateTime.Now.Month - 1; // current month
+        }
+
+        if (selectedYear > DateTime.Now.Year || selectedYear == null)
+        {
+            selectedYear = DateTime.Now.Year; // current year
+        }
+        var plannedShifts = Context.Shifts.Where(s => s.Start.Month == selectedMonth && s.Start.Year == selectedYear).ToList();
+        var workedHours = Context.WorkedHours.Where(wh => wh.DateOnly.Month == selectedMonth && wh.DateOnly.Year == selectedYear).ToList();
+
+        var amountOfPlannedHours = 0;
+        var amountOfWorkedHours = 0;
+
+        var viewModel = new MonthlyHoursViewModel
+        {
+            SelectedMonth = (int)selectedMonth,
+            SelectedYear = (int)selectedYear,
+            PlannedHours = amountOfPlannedHours,
+            WorkedHours = amountOfWorkedHours,
+            Difference = amountOfWorkedHours - amountOfPlannedHours
+        };
+        return View(viewModel);
     }
     
     [HttpGet]
---
+++ ViewModels/MonthlyHoursViewModel.cs
@@ -0,0 +1,15 @@
+﻿using BumboApp.Models;
+
+namespace BumboApp.ViewModels
+{
+    public class MonthlyHoursViewModel
+    {
+        public required int SelectedMonth { get; set; }
+        public required int SelectedYear { get; set; }
+        public required int WorkedHours { get; set; }
+        public required int PlannedHours { get; set; }
+        public required int Difference {  get; set; }
+
+        public string[] Months = ["Januari", "Februari", "Maart", "April", "Mei", "Juni", "Juli", "Augustus", "September", "Oktober", "November", "December"];
+    }
+}
